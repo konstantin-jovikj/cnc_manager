0dc0aae00e76d6e3cf43388b26ccebb4527c0a9e
konstantin-jovikj <jovichmk@gmail.com>
2023-09-14 13:21:35 +0200

    Creating basic models, controllers amd migrations

---
+++ app/Http/Controllers/UsedToolController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\UsedTool;
+use Illuminate\Http\Request;
+
+class UsedToolController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(UsedTool $usedTool)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(UsedTool $usedTool)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, UsedTool $usedTool)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(UsedTool $usedTool)
+    {
+        //
+    }
+}
