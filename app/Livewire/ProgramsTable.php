feaf15a138db9a4b7a1dd77502e4fdd5e01e5cb3
konstantin-jovikj <jovichmk@gmail.com>
2023-09-18 15:57:40 +0200

    Programs table wivewire component created

---
+++ app/Livewire/ProgramsTable.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class ProgramsTable extends Component
+{
+    public function render()
+    {
+        return view('livewire.programs-table');
+    }
+}
